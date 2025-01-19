b9e83f6468f27c7248fd534f9d0358abb3692dc0
Tyler Nardone <tynardone@gmail.com>
2025-01-19 18:08:35 -0500

    Created a command

--- cli.py
+++ cli.py
@@ -1,51 +1,31 @@
 import click
-from dataclasses import dataclass
+
 from brewery import random_brewery
+from models import Brewery
 
 
-@dataclass
-class Coordinate:
-    longitude: int
-    latitude: int
-
-
-@dataclass
-class Address:
-    address_one: str
-    address_two: str | None
-    address_three: str | None
-    street: str
-    city: str
-    state: str
-    posta_code: str
-    country: str
-    coordinate: Coordinate
-
-
-@dataclass
-class Brewery:
-    id: str
-    name: str
-    address: Address
-    phone: str
-    website_url: str
-
-
-@click.command()
-@click.argument("number")
-def main(number) -> None:
+@click.group()
+def cli() -> None:
     """
     A simple CLI that retrieves random breweries and displays their name, location,
     and a link to their website.
 
     Provide a number specifying how many results you would like!
     """
-    response: list[dict] = random_brewery(number=number)
-    for result in response:
-        click.echo(
-            f"{result.get("name")} located in {result.get("city")}, {result.get("state")}. Learn more at {result.get("website_url")}"
-        )
+    pass
+
+
+@cli.command()
+@click.argument("number")
+def random(number):
+    breweries = [
+        Brewery.from_dict(brewery) for brewery in random_brewery(number=number)
+    ]
+
+    for result in breweries:
+        click.echo(result)
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    cli()
---
+++ models.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass, fields
+
+
+@dataclass
+class Coordinate:
+    longitude: float | None
+    latitude: float | None
+
+    def __post_init__(self):
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if value is not None:
+                try:
+                    setattr(self, field.name, float(value))
+                except ValueError:
+                    raise ValueError(f"Cannot convert {field.name}={value!r} to float")
+
+
+@dataclass
+class Address:
+    address_one: str
+    address_two: str | None
+    address_three: str | None
+    street: str
+    city: str
+    state: str
+    posta_code: str
+    country: str
+    coordinate: Coordinate
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Address":
+        coordinate = Coordinate(
+            longitude=data.get("longitude"), latitude=data.get("latitude")
+        )
+        return cls(
+            address_one=data["address_1"],
+            address_two=data["address_2"],
+            address_three=data["address_3"],
+            street=data["street"],
+            city=data["city"],
+            state=data["state"],
+            posta_code=data["postal_code"],
+            country=data["country"],
+            coordinate=coordinate,
+        )
+
+
+@dataclass
+class Brewery:
+    id: str
+    name: str
+    address: Address
+    phone: str
+    website_url: str
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Brewery":
+        address = Address.from_dict(data)
+        return cls(
+            id=data["id"],
+            name=data["name"],
+            address=address,
+            phone=data["phone"],
+            website_url=data["website_url"],
+        )
